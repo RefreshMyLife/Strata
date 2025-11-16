60ad00d51e051f5a7cda2ca2a19be58262aebe3c
RefreshMyLife <andreuzakharoy777@gmail.com>
2025-11-16 15:48:06 +0300

    fix: IPhone margin

--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -84,7 +84,7 @@ export const useStyles = () => {
             /* Стили для iOS устройств (включая iPhone X) */
             @supports (-webkit-touch-callout: none) {
                 ${theme.breakpoints.down('sm')} {
-                    margin-bottom: 20px;
+                    margin-bottom: 80px;
                     /* Добавляем padding для безопасной зоны (notch) */
                     padding-bottom: env(safe-area-inset-bottom);
                 }
